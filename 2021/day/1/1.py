2f61a882a2adb2369768a9dd5830f3ad2483c767
ChrisCodeSmith <67827118+ChrisCodeSmith@users.noreply.github.com>
2021-12-09 09:01:14 +0100

    refactor: code readability and description

--- 2021/day/1/1.py
+++ 2021/day/1/1.py
@@ -1,15 +1,15 @@
-# We read the lines from input.txt and iterate over every line and compare it to a tmp var, which is initially 0,
-# but gets updated at the end of every iteration with its measurement.
-# So after the first iteration it always contains the measurement of the previous iteration.
+# We read the lines from input.txt and iterate over every line and compare it to a tmp var, which is initially maxint.
+# This tmp var gets updated to the actual measurement at the end of each iteration,
+# so it has the value of the previous iteration for all calcualtions in the next iteration.
 #
-# The counter increases everytime the actual measurement is greater than the one before.
+# The counter increases everytime the actual measurement is greater than the one before: measurement > tmp
 
-
+import sys
 with open('input.txt', 'r') as f:
     measurements = f.read().splitlines()
 
-    counter = -1  # -1 becuz the first element is smaller than 0 and we count it even thou we shouldn't
-    tmp = 0
+    counter = 0
+    tmp = sys.maxsize
     for measurement in measurements:
         if int(measurement) > tmp:
             counter += 1
--- 2021/day/1/2.py
+++ 2021/day/1/2.py
@@ -1,17 +1,18 @@
+import sys
 with open('input.txt', 'r') as f:
     measurements = f.read().splitlines()
 
     # This is the main thing of part 2:
     #
-    # We solve it with a list comprehension: we loop over the origin list and sum the numbers from i to i+3
+    # We solve it with a list comprehension: we loop over the origin list and sum the numbers from i to i+3 for every i in measurements.
     # We use the map function inside the sum function to convert the strings to integers.
 
     triple_mes = [sum(map(int, measurements[i:i+3]))
                   for i in range(0, len(measurements), 1)]
 
     # we copy the counter from the first part of day 1 - we could write a reusable function, but since we only use it twice and its only for one challenge, we are fine.
-    counter = -1
-    tmp = 0
+    counter = 0
+    tmp = sys.maxsize
     for measurement in triple_mes:
         if int(measurement) > tmp:
             counter += 1
